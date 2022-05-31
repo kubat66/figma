69f7107409f92434c9de2d7242b72c8576d56b37
kubat66 <kubatmir3@gmail.com>
2022-05-31 15:12:56 +0600

    initial

---
+++ js/swip.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".mySwiper", {
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+        },
+        next: {
+            translate: ["100%", 0, 0],
+        },
+    },
+});
